e0c5dc5333df054fada860280c0493c055a9c22a
Kelvin <duchung97dl@gmail.com>
2019-09-07 14:33:38 +1000

    fetch api

--- govhack2019/src/App.js
+++ govhack2019/src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import { Row, Col, Button } from "react-bootstrap";
 import { Map, GoogleApiWrapper } from "google-maps-react";
 
 export class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    };
+  }
+  fecthData = async () => {
+    try {
+      const res = await axios(
+        `https://data.gov.au/geoserver/qld-suburb-locality-boundaries-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_6bedcb55_1b1f_457b_b092_58e88952e9f0&outputFormat=json`
+      );
+      this.setState({ result: res });
+      console.log(this.state.result);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   render() {
     const mapStyles = {
       width: "100%",
@@ -15,6 +35,7 @@ export class App extends Component {
           <Col md={3} xs={3} style={{ backgroundColor: "lightblue" }}>
             1
           </Col>
+          <Button onClick={this.fecthData}></Button>
           <Col
             md={7}
             xs={7}
