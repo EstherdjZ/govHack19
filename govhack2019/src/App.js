7b561d327fe48b660450834c6365a335b5adba9c
- <yuntung.shen@uqconnect.edu.au>
2019-09-07 13:54:29 +1000

    no

--- govhack2019/src/App.js
+++ govhack2019/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Row, Col } from "react-bootstrap";
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 class App extends Component {
   initMap = async () => {
+    google = {props.google}
     const map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: -27.46794, lng: 153.02809 },
       zoom: 8
@@ -21,13 +23,22 @@ class App extends Component {
 
   render() {
     return (
+
+      <Map
+          google={this.props.google}
+          zoom={8}
+          center={lat: -27.46794, lng: 153.02809}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
+
       <div>
         <Row>
           <Col md={4} xs={4} style={{ backgroundColor: "lightblue" }}>
             1
           </Col>
           <Col md={8} xs={8} style={{ backgroundColor: "lightblue" }}>
-            <div id="map"></div>
+            <div id="map">conponentDidMount()</div>
           </Col>
         </Row>
       </div>
