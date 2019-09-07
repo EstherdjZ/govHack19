f5fe01d20dcc8c3e2b0bcd6967c47694c0b56179
wulaiyin789 <wulaiyin789@gmail.com>
2019-09-07 13:30:43 +1000

    Updated

--- govhack2019/src/App.js
+++ govhack2019/src/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Row, Col } from "react-bootstrap";
+import {Map, GoogleApiWrapper} from 'google-maps-react';
 
-class App extends Component {
+export class App extends Component {
   render() {
+      const mapStyles = {
+          width: '100%',
+          height: '100%'
+      };
     return (
       <div>
         <Row>
           <Col md={4} xs={4} style={{ backgroundColor: "lightblue" }}>
             1
           </Col>
-          <Col md={8} xs={8} style={{ backgroundColor: "lightblue" }}>
-            2 of 2
-          </Col>
         </Row>
+              <Map
+                  google={this.props.google}
+                  zoom={14}
+                  style={mapStyles}
+                  initialCenter={{
+                      lat: -1.2884,
+                      lng: 36.8233
+                  }}
+              />
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyBL6mg5kAFhttbqmVLxl4ikklsb6eQbDHE'
+})(App);
