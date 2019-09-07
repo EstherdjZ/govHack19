eea6de430a1ed3e2112da9a170103ab331652e33
Kelvin <duchung97dl@gmail.com>
2019-09-07 13:38:00 +1000

    change some stupid things

--- govhack2019/src/App.js
+++ govhack2019/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Row, Col } from "react-bootstrap";
+import { withGoogleMap, GoogleMap, Polygon } from "react-google-maps";
 
 class App extends Component {
-  initMap = async () => {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -27.46794, lng: 153.02809 },
-      zoom: 8
-    });
-    map.data.loadGeoJson(
-      "https://data.gov.au/geoserver/qld-suburb-locality-boundaries-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_6bedcb55_1b1f_457b_b092_58e88952e9f0&outputFormat=json"
-    );
-  };
+  // initMap = () => {
+  //   const map = new google.maps.Map(document.getElementById("map"), {
+  //     center: { lat: -27.46794, lng: 153.02809 },
+  //     zoom: 8
+  //   });
+  //   map.data.loadGeoJson(
+  //     "https://data.gov.au/geoserver/qld-suburb-locality-boundaries-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_6bedcb55_1b1f_457b_b092_58e88952e9f0&outputFormat=json"
+  //   );
+  // };
 
   conponentDidMount() {
     const api = fetch(
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBL6mg5kAFhttbqmVLxl4ikklsb6eQbDHE&callback=initMap"
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBL6mg5kAFhttbqmVLxl4ikklsb6eQbDHE&callback=`${this.initMap}`"
     );
+    console.log(api);
   }
 
   render() {
