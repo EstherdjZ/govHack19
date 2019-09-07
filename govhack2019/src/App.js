d4c1dd75b461794b1b105baddbe43816f8f465d8
Kelvin <duchung97dl@gmail.com>
2019-09-07 13:55:24 +1000

    fix map

--- govhack2019/src/App.js
+++ govhack2019/src/App.js
@@ -1,39 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Row, Col } from "react-bootstrap";
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 export class App extends Component {
-
   render() {
-      const mapStyles = {
-          width: '100%',
-          height: '100%'
-      };
+    const mapStyles = {
+      width: "100%",
+      height: "100%"
+    };
     return (
       <div>
         <Row>
-          <Col md={4} xs={4} style={{ backgroundColor: "lightblue" }}>
+          <Col md={3} xs={3} style={{ backgroundColor: "lightblue" }}>
             1
           </Col>
-          <Col md={8} xs={8} style={{ backgroundColor: "lightblue" }}>
-            <div id="map"></div>
+          <Col
+            md={7}
+            xs={7}
+            style={{ backgroundColor: "lightblue", position: "inherit" }}
+          >
+            <Map
+              google={this.props.google}
+              zoom={14}
+              style={mapStyles}
+              initialCenter={{
+                lat: -27.46794,
+                lng: 153.02809
+              }}
+            />
           </Col>
         </Row>
-              <Map
-                  google={this.props.google}
-                  zoom={14}
-                  style={mapStyles}
-                  initialCenter={{
-                      lat: -1.2884,
-                      lng: 36.8233
-                  }}
-              />
       </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyBL6mg5kAFhttbqmVLxl4ikklsb6eQbDHE'
+  apiKey: "AIzaSyBL6mg5kAFhttbqmVLxl4ikklsb6eQbDHE"
 })(App);
