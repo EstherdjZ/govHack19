82723d1be608211d9cf76e9bafe391999e83e2f9
- <yuntung.shen@uqconnect.edu.au>
2019-09-07 14:32:54 +1000

    a

--- govhack2019/src/App.js
+++ govhack2019/src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Row, Col } from "react-bootstrap";
-<<<<<<< HEAD
+
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class App extends Component {
-  initMap = async () => {
-    google = {props.google}
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -27.46794, lng: 153.02809 },
-      zoom: 8
-    });
-    map.data.loadGeoJson(
-      "https://data.gov.au/geoserver/qld-suburb-locality-boundaries-psma-administrative-boundaries/wfs?request=GetFeature&typeName=ckan_6bedcb55_1b1f_457b_b092_58e88952e9f0&outputFormat=json"
-    );
-  };
 
-  conponentDidMount() {
-    const api = fetch(
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBL6mg5kAFhttbqmVLxl4ikklsb6eQbDHE&callback=initMap"
-    );
-  }
-=======
-import {Map, GoogleApiWrapper} from 'google-maps-react';
 
 export class App extends Component {
->>>>>>> f3586f01ab21f21c5c566ac0677423be681da8a2
 
   render() {
       const mapStyles = {
@@ -34,13 +15,7 @@ export class App extends Component {
       };
     return (
 
-      <Map
-          google={this.props.google}
-          zoom={8}
-          center={lat: -27.46794, lng: 153.02809}
-          style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        />
+
 
       <div>
         <Row>
@@ -48,7 +23,7 @@ export class App extends Component {
             1
           </Col>
           <Col md={8} xs={8} style={{ backgroundColor: "lightblue" }}>
-            <div id="map">conponentDidMount()</div>
+            <div id="map"></div>
           </Col>
         </Row>
               <Map
